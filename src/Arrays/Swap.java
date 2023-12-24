235a84239fdcfc25844c91f9266d012dc7021d25
ishshah1101 <isha.techcareer@gmail.com>
2023-12-24 16:55:02 -0600

    Initial Commit

---
+++ src/Arrays/Swap.java
@@ -0,0 +1,14 @@
+package Arrays;
+
+public class Swap {
+
+    public static void main(String[] args) {
+        swapNumbers(0, 1);
+    }
+
+    private static void swapNumbers(int a, int b) {
+        // TODO: LOGIC
+        System.out.println("a::--->" + a + "b::--->" + b);
+    }
+
+}
